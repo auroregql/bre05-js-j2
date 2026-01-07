7561980e54cc16a96deaacb073faa249e8a7a956
auroregql <aurore.gicquel.06@gmail.com>
2026-01-07 09:10:40 +0000

    Exercice 2 Terminé

---
+++ exercices-tableaux/exercice2/index.js
@@ -0,0 +1,17 @@
+let animals_even = ["Dog", "Cat", "Spider"];
+
+for(let i = 0; i < animals_even.length; i++)
+{
+    if (i % 2 === 0) {
+        console.log(animals_even[i])
+    }
+}
+
+let animals_odd = ["Bat", "Snake", "Bear"];
+
+for(let i = 0; i < animals_odd.length; i++)
+{
+    if (i % 2 !== 0) {
+        console.log(animals_odd[i])
+    }
+}
